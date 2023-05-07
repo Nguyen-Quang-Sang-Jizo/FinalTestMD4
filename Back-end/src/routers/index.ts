551f3563d60415eed1924c8e8fa4d1ca1549485e
Nguyen-Quang-Sang-Jizo <117348347+Nguyen-Quang-Sang-Jizo@users.noreply.github.com>
2023-05-07 21:26:49 +0700

    update

--- Back-end/src/routers/index.ts
+++ Back-end/src/routers/index.ts
@@ -1,7 +1,10 @@
 import userRouter from "./user.router";
+import postRouter from "./postRouter";
 
 const router = (app) => {
     app.use('/auth', userRouter);
+    app.use('/post', postRouter);
+
 };
 
 export default router;
